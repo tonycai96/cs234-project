1f9998426b64e4f15161e6b83a3228a69fca340d
Tony Cai <>
2023-03-06 00:46:52 -0800

    Fix baseline1 (performance not very good probably because most features are unknown)

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def fixed_actions(dataset):
 
 def clinical_dose(dataset):
     dosage_df = dataset.copy()
-    dosage_df['Predicted Dose'] = 4.0376 + 0.2546*dataset['_age_decade'] + 0.0118*dataset['Height (cm)'] + 0.0134*dataset['Weight (kg)'] \
+    dosage_df['Predicted Dose'] = 4.0376 - 0.2546*dataset['_age_decade'] + 0.0118*dataset['Height (cm)'] + 0.0134*dataset['Weight (kg)'] \
         - 0.6752*dataset['_race_asian'] + 0.4060*dataset['_race_black'] + 0.0443*dataset['_race_other'] \
         - 0.5695*dataset['Amiodarone (Cordarone)']
     enzyme_inducer_active = (dosage_df['Carbamazepine (Tegretol)'] == 1.0) | (dosage_df['Phenytoin (Dilantin)'] == 1.0) | (dosage_df['Rifampin or Rifampicin'] == 1.0)
@@ -93,7 +93,7 @@ if __name__ == "__main__":
     clinical_dose_acc = clinical_dose(patients_df)
     print('Clinical dose accuracy = ', clinical_dose_acc)
 
-    Part (2)
+    # Part (2)
     features, arms_rewards = compute_features_and_targets(patients_df)
     linucb_dose = linear_ucb(features, arms_rewards, alpha=1)
 
@@ -105,3 +105,45 @@ if __name__ == "__main__":
     timesteps = np.linspace(0, n_patients, n_patients+1)
     plt.plot(timesteps, regrets)
     plt.show()
+
+
+'''
+Index(['PharmGKB Subject ID', 'Gender', 'Race', 'Ethnicity', 'Age',
+       'Height (cm)', 'Weight (kg)', 'Indication for Warfarin Treatment',
+       'Comorbidities', 'Diabetes',
+       'Congestive Heart Failure and/or Cardiomyopathy', 'Valve Replacement',
+       'Medications', 'Aspirin', 'Acetaminophen or Paracetamol (Tylenol)',
+       'Was Dose of Acetaminophen or Paracetamol (Tylenol) >1300mg/day',
+       'Simvastatin (Zocor)', 'Atorvastatin (Lipitor)', 'Fluvastatin (Lescol)',
+       'Lovastatin (Mevacor)', 'Pravastatin (Pravachol)',
+       'Rosuvastatin (Crestor)', 'Cerivastatin (Baycol)',
+       'Amiodarone (Cordarone)', 'Carbamazepine (Tegretol)',
+       'Phenytoin (Dilantin)', 'Rifampin or Rifampicin',
+       'Sulfonamide Antibiotics', 'Macrolide Antibiotics',
+       'Anti-fungal Azoles', 'Herbal Medications, Vitamins, Supplements',
+       'Target INR', 'Estimated Target INR Range Based on Indication',
+       'Subject Reached Stable Dose of Warfarin',
+       'Therapeutic Dose of Warfarin',
+       'INR on Reported Therapeutic Dose of Warfarin', 'Current Smoker',
+       'Cyp2C9 genotypes', 'Genotyped QC Cyp2C9*2', 'Genotyped QC Cyp2C9*3',
+       'Combined QC CYP2C9',
+       'VKORC1 genotype: -1639 G>A (3673); chr16:31015190; rs9923231; C/T',
+       'VKORC1 QC genotype: -1639 G>A (3673); chr16:31015190; rs9923231; C/T',
+       'VKORC1 genotype: 497T>G (5808); chr16:31013055; rs2884737; A/C',
+       'VKORC1 QC genotype: 497T>G (5808); chr16:31013055; rs2884737; A/C',
+       'VKORC1 genotype: 1173 C>T(6484); chr16:31012379; rs9934438; A/G',
+       'VKORC1 QC genotype: 1173 C>T(6484); chr16:31012379; rs9934438; A/G',
+       'VKORC1 genotype: 1542G>C (6853); chr16:31012010; rs8050894; C/G',
+       'VKORC1 QC genotype: 1542G>C (6853); chr16:31012010; rs8050894; C/G',
+       'VKORC1 genotype: 3730 G>A (9041); chr16:31009822; rs7294;  A/G',
+       'VKORC1 QC genotype: 3730 G>A (9041); chr16:31009822; rs7294;  A/G',
+       'VKORC1 genotype: 2255C>T (7566); chr16:31011297; rs2359612; A/G',
+       'VKORC1 QC genotype: 2255C>T (7566); chr16:31011297; rs2359612; A/G',
+       'VKORC1 genotype: -4451 C>A (861); Chr16:31018002; rs17880887; A/C',
+       'VKORC1 QC genotype: -4451 C>A (861); Chr16:31018002; rs17880887; A/C',
+       'CYP2C9 consensus', 'VKORC1 -1639 consensus', 'VKORC1 497 consensus',
+       'VKORC1 1173 consensus', 'VKORC1 1542 consensus',
+       'VKORC1 3730 consensus', 'VKORC1 2255 consensus',
+       'VKORC1 -4451 consensus', 'Unnamed: 63', 'Unnamed: 64', 'Unnamed: 65'],
+      dtype='object')
+'''
